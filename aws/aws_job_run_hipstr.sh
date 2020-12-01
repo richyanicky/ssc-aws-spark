d3ce468287b63da59b36cc3b376a6be563b4d1ed
richyanicky <richyanicky@gmail.com>
2020-12-01 13:34:12 -0800

    updated hipstr settings

--- aws/aws_job_run_hipstr.sh
+++ aws/aws_job_run_hipstr.sh
@@ -25,7 +25,7 @@ aws s3 cp ../${SSC_PARAMS} s3://ssc-hipstr-spark/scripts/ || echo "${SSC_PARAMS}
 aws s3 cp ../pipeline/run_hipstr.sh s3://ssc-hipstr-spark/scripts/ || echo "run_gangstr_chrx.sh upload to s3 failed"
 aws s3 cp ../src/decrypt.py s3://ssc-hipstr-spark/scripts/ || echo "decrypt.py upload to s3 failed"
 aws s3 cp ../files/encrypt_code.txt s3://ssc-hipstr-spark/scripts/encrypt_code.txt ||  echo "encrypt_code.txt upload to s3 failed"
-aws s3 cp ../files/ssc_crams_sample_data.csv s3://ssc-hipstr/spark/scripts/ssc_crams_sample_data.csv ||  echo "ssc_crams_sample_data.csv upload to s3 failed"
+aws s3 cp ../files/ssc_crams_sample_data.csv s3://ssc-hipstr-spark/scripts/ssc_crams_sample_data.csv ||  echo "ssc_crams_sample_data.csv upload to s3 failed"
 
 
 SSC_ACCESS_KEY=$(cat ~/.aws/credentials | grep -A 2 ssc2 | grep id | cut -f 2 -d '=' | cut -f 2 -d' ' )
@@ -40,7 +40,7 @@ while read -r FAMID CRAMSLIST; do
   aws batch submit-job \
       --job-name ssc-${FAMID} \
       --job-queue ssc-denovo \
-      --job-definition SSC-advntr:14 \
-      --container-overrides 'command=["run_advntr.sh",'"${CRAMSLIST}"','"${FAMID}"','"${ENC_SSC_ACCESS_KEY}"','"${ENC_SSC_SECRET_ACCESS_KEY}"', '"${SSC_PARAMS}"'],environment=[{name="BATCH_FILE_TYPE",value="script"},{name="BATCH_FILE_S3_URL",value="s3://ssc-advntr-denovos/scripts/run_advntr.sh"}]'
+      --job-definition ssc-hipstr-spark:2 \
+      --container-overrides 'command=["run_hipstr.sh",'"${CRAMSLIST}"','"${FAMID}"','"${ENC_SSC_ACCESS_KEY}"','"${ENC_SSC_SECRET_ACCESS_KEY}"', '"${SSC_PARAMS}"'],environment=[{name="BATCH_FILE_TYPE",value="script"},{name="BATCH_FILE_S3_URL",value="s3://ssc-advntr-denovos/scripts/run_advntr.sh"}]'
 
 done <${SSC_FILE}
